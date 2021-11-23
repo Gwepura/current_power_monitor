798415bc8d9e0a6821af4640af28358a0c01f45f
Gwepura <aketani32@gmail.com>
2021-11-23 20:56:39 +0200

    Remove timestamp from save_data.php

--- save_data.php
+++ save_data.php
@@ -2,10 +2,13 @@
   // Load database configuration file
   include_once 'db_config.php';
   
-  if ($_GET["timestamp"] && $_GET["current"] &&
-    $_GET["voltage"] && $_GET["power"] && $_GET["flow_rate"]) {
-      $sql = "INSERT into data_readings (data_timestamp,current,voltage,data_power,flow_rate)
-              values('".$_GET["timestamp"]."','".$_GET["current"]."','".$_GET["voltage"]."','".$_GET["power"]."','".$_GET["flow_rate"]."')";
-              $result = mysqli_query($conn, $sql);
+  if ($_GET["current"] && $_GET["voltage"] && $_GET["power"] && $_GET["flow_rate"]) {
+    save_row($_GET["current"], $_GET["voltage"], $_GET["power"], $_GET["flow_rate"], $conn);
+  }
+
+  function save_row($current, $voltage, $data_power, $flow_rate, $conn) {
+    $sql = "INSERT into data_readings (current,voltage,data_power,flow_rate)
+              values('".$current."','".$voltage."','".$data_power."','".$flow_rate."')";
+    $result = mysqli_query($conn, $sql);
   }
 ?>
