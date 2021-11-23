c8b2e31ca0ebd5c091fbcedc7f82863b82c7d742
Gwepura <aketani32@gmail.com>
2021-11-23 12:10:21 +0200

    Add delete button and touch up css

---
+++ delete_data.php
@@ -0,0 +1,13 @@
+<?php 
+  // Load database configuration file
+  include_once 'db_config.php'; 
+  
+  // Get all records from database
+  $sql = "DELETE FROM data_readings";
+  $result = mysqli_query($conn, $sql);
+
+  $_SESSION['redirect_url'] = $_SERVER['PHP_SELF']; 
+  header('Location: index.html');
+
+  exit;
+?>
